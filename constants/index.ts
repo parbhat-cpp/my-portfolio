7b028a235f0665f1b9c8bc6bbae820ca239533f8
Parbhat Sharma <83265789+parbhat-cpp@users.noreply.github.com>
2026-05-10 15:57:02 +0530

    update resume link

--- constants/index.ts
+++ constants/index.ts
@@ -174,7 +174,7 @@ export const projects = [
 ];
 
 export const resumeLink =
-  "https://drive.google.com/file/d/1w0tjeqHGi65aKQSruahDvMKpCWSOdIMh/view?usp=sharing";
+  "https://drive.google.com/file/d/1J8nDJnfdbUSGm82ORi283zc6vlxfEZc_/view?usp=sharing";
 
 export const metaKeywords = [
   "Parbhat Sharma",
